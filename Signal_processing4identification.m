0060e9786ae41d04a22ea87666ab18d9ccf7e9c0
patakibalazs03 <pataki.balazs03@gmail.com>
2025-11-07 01:31:00 +0100

    Measurements complete

--- Signal_processing4identification.m
+++ Signal_processing4identification.m
@@ -5,14 +5,24 @@ Ido2 = Kerek2_elore.Time;
 Output1 = squeeze(Kerek1_elore.Data);
 Output2 = squeeze(Kerek2_elore.Data);
 
+% Ido = linspace(0,25);
+% Ido1 = Ido; Ido2 = Ido;
+% Input = sqrt(Ido);
+% Output1 = 2*Ido;
+% Output2 = Ido;
+
 figure(1);
 hold on;
+title("Identifikáció");
+yyaxis left;
 plot(Ido,Input);
-plot(Ido1,Output1);
-plot(Ido2,Output2);
-legend('Bemenet [%]','Bal oldali kerék sebessége [rad/s]','Jobb oldali kerék sebessége [rad/s]');
 xlabel("Idő [s]");
-ylabel("Bemeneti jel [%]/Kerék sebesség [rad/s]");
+ylabel("Bemeneti jel [%]");
+yyaxis right;
+plot(Ido1,Output1,"-.r");
+plot(Ido2,Output2,"-r");
+ylabel("Kerekek sebessége [rad/s]");
+legend('Bemenet [%]','Bal oldali kerék sebessége [rad/s]','Jobb oldali kerék sebessége [rad/s]');
 legend("boxoff");
 legend(location="northwest");
 hold off;
