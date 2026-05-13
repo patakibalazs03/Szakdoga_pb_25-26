f5603dbe5123b658b6d5dc35ad58e28584b398ad
patakibalazs03 <pataki.balazs03@gmail.com>
2026-05-13 16:42:22 +0200

    ColorSensor_half_implemented

--- C_kod/SensorActuator.c
+++ C_kod/SensorActuator.c
@@ -1,37 +1,59 @@
 #include "SensorActuator.h"
-// Everything that needs to be hidden from simulink like hardware stuff goes here
+////////////////////////////////////////////////
+//// Hardware includes and defines go here ////
+//////////////////////////////////////////////
 #if !defined(SLCC_SIMULATION) && !defined(MATLAB_MEX_FILE)
     #include "main.h"
     
     extern ADC_HandleTypeDef hadc1;
     extern TIM_HandleTypeDef htim4;
+    extern TIM_HandleTypeDef htim5;
     
     #define VBAT_ADC (&hadc1)
 #endif
 
-//Global Variables
+  ///////////////////////////
+ //// Global Variables /////
+///////////////////////////
 
-infraredTypedef InfraSensor = { .clk_MHz = 100, .PSC = 999 };    //PSC and clk_MHz written from the initialization!!! Changing it here doesnt do anything! Reason: robust code, less "mouse clicks" :)
+// The PSC is set to 1000, because it gives 100 kHz, which gives the ideal resolution s timer overflow
+infraredTypedef InfraSensor = { .clk_MHz = 100, .PSC = 999 };    //PSC and clk_MHz also written from the initialization!
 uint32_t last_distance = 400; // The default should be 40 centimeters, because I usually set the target to 40 cm-s
 uint32_t batteryVoltage = 0;  //
+uint32_t last_speed = 0;
 
+volatile encoderTypedef Encoder = { .clk_Hz = 100000000, .PSC = 0, .PSCR1 = 1, .PSCR3 = 1, .CPR = 1024};
+volatile colorSensorTypedef ColorSensor = {.Color = COLOR_RED, .stepFlag = false};
 
-
+/////////////////////////////////////////////
+//// All the hardware interrupts go here ///
+///////////////////////////////////////////
 #if !defined(SLCC_SIMULATION) && !defined(MATLAB_MEX_FILE)
-// HAL_TIM_IC_CaptureCallback
-// Overdefines the standard HAL interrupt callback; handles all the interupts of timer input capture modes
+//----------------------------//
+// HAL_TIM_IC_CaptureCallback //
+//----------------------------//
+/*
+Overdefines the standard HAL interrupt callback; handles all the interupts of timer input capture modes
+Takes the handle
+*/
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim){
-//#ifndef MATLAB_MEX_FILE
-    // Check the timer instance
+    // ---------------------------------------------------------
+    // Timer 4
+    // ---------------------------------------------------------
     if (htim->Instance == TIM4) 
     {
-        // Check the timer channel
+        // ---------------------------------------------------------
+        // CHANNEL 2 (Distance measurement, rising edge)
+        // ---------------------------------------------------------
         if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_2) 
         {
             // Read the captured value of the rising edge
             uint16_t captured_value = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_2);
             InfraSensor.rise_capture_value = captured_value;
         }
+        // ---------------------------------------------------------
+        // CHANNEL 1 (Distance measurement, rising edge)
+        // ---------------------------------------------------------
         else if(htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)
         {
             // Read the capture value of the falling edge
@@ -39,20 +61,167 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim){
             InfraSensor.fall_capture_value = captured_value;
             // Calculate the difference, the timer is 16 bit, thus the difference is 16 bit and the captured values are 16 bit
             uint16_t diff = InfraSensor.fall_capture_value - InfraSensor.rise_capture_value;
+            // Just in case the register doesn't update
+            if(diff == 0) return;
              // Calculate the distance [mm]
             InfraSensor.distance_mm = ((uint32_t)diff * (InfraSensor.PSC+1) / (InfraSensor.clk_MHz*10)); 
             // From left to right(*almost): we get the difference, which we divide with the clk_MHz/(PSC+1), thus the PSC+1 goes into the numerator, then we get the time elapsed in us, so the scaling constant is 100
             // I have reordered it, so the multiplication comes before the division, thus it is more efficient; also casting the variable to prevent overflow
         }
+        // ---------------------------------------------------------
+        // CHANNEL 3 (Color Sensor,)
+        // ---------------------------------------------------------
+        else if(htim->Channel == HAL_TIM_ACTIVE_CHANNEL_3)
+        {
+            ColorSensor.capture_value = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_3);
+            uint16_t diff = ColorSensor.capture_value - ColorSensor.prev_capture_value;
+            ColorSensor.prev_capture_value = ColorSensor.capture_value;
+
+            if(diff == 0) return;
+
+            ColorSensor.counter++;
+
+            if(ColorSensor.counter >= 20)
+            {
+                float total_ticks = (float)ColorSensor.sum * (1.0f + (float)ColorSensor.PSC);
+                float period = total_ticks / (ColorSensor.clk_Hz * 16.0f); 
+                if(ColorSensor.Color == COLOR_RED)
+                {
+                    ColorSensor.pRed =  period;
+                    ColorSensor.Color = COLOR_BLUE;
+                    LL_GPIO_SetOutputPin(GPIOB, LL_GPIO_PIN_6);
+                    ColorSensor.counter = 0;
+                    ColorSensor.sum = 0;
+                }
+                else if(ColorSensor.Color == COLOR_BLUE)
+                {
+                    ColorSensor.pBlue = period;
+                    ColorSensor.Color = COLOR_GREEN;
+                    LL_GPIO_SetOutputPin(GPIOB, LL_GPIO_PIN_5);
+                    ColorSensor.counter = 0;
+                    ColorSensor.sum = 0;
+                }
+                else if(ColorSensor.Color == COLOR_GREEN)
+                {
+                    ColorSensor.pGreen = period;
+                    ColorSensor.Color = COLOR_RED;
+                    LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_5);
+                    LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_6);
+                    ColorSensor.counter = 0;
+                    ColorSensor.sum = 0;
+                }
+
+                ColorSensor.counter = 0;
+                ColorSensor.sum = 0;  
+            }
+            else if(ColorSensor.counter >= 4)
+            { 
+                ColorSensor.sum += diff;
+            }
+        }
     }
-//#endif
+    // ---------------------------------------------------------
+    // Timer 5
+    // ---------------------------------------------------------
+    else if(htim->Instance == TIM5)
+    {
+        // ---------------------------------------------------------
+        // CHANNEL 1 (ENCODER A)
+        // ---------------------------------------------------------
+        if(htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)
+        {
+            // Reset the Encoder.watchdog_timerA, which takes the speed to 0 if there are no interrupts
+            Encoder.watchdog_timerA = 0;
+            // Calculate the difference of the timer counter between two rising edges
+            Encoder.capture_valueA = HAL_TIM_ReadCapturedValue(htim,TIM_CHANNEL_1);
+            uint32_t diff = Encoder.capture_valueA-Encoder.prev_capture_valueA;
+            Encoder.prev_capture_valueA = Encoder.capture_valueA;
+            // If there has been an error, return
+            if(diff == 0) return;
+            // Decide if the motor is running on low or high speed, switching takes 1 clock cycle, which will be a wrong value, so we don't calculate with it
+            if(Encoder.PSCR1 == 1 && !Encoder.skip_next_calcA)
+            {
+                // speed in [m/s] = 2*pi*f_clk/((1+PSC)*diff*CPR)
+                Encoder.speed_mpsA = Encoder.clk_Hz * 2.0f * PI * WHEEL_DIAMETER / (1+Encoder.PSC) / diff / Encoder.CPR;
+            }
+            else if(Encoder.PSCR1 == 8 && !Encoder.skip_next_calcA)
+            {
+                // on high speed we only calculate the time it takes to make 8 edges, so the processor isn't overwhelmed
+                Encoder.speed_mpsA = Encoder.clk_Hz * 2.0f * PI * WHEEL_DIAMETER * 8 / (1+Encoder.PSC) / diff / Encoder.CPR;
+            }
+            else
+            {
+                // reset the false data flag
+                Encoder.skip_next_calcA = false;
+            }
+
+            // For the direction, we simply watch, whether the other channel is HIGH/LOW in the time of this one
+            Encoder.dirA = LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_1) ? 1 : 0;
+
+            // Logic setting the timer channel division prescaler
+            if(Encoder.speed_mpsA > HIGH_SPEED_THRESHOLD && Encoder.PSCR1 == 1)
+            {
+                //High speed
+                    __HAL_TIM_SET_ICPRESCALER(htim, TIM_CHANNEL_1, TIM_ICPSC_DIV8);
+                    Encoder.PSCR1 = 8;
+                    Encoder.skip_next_calcA = true;
+            }
+            else if(Encoder.speed_mpsA < LOW_SPEED_THRESHOLD && Encoder.PSCR1 == 8)
+            {
+                //Low speed
+                    __HAL_TIM_SET_ICPRESCALER(htim, TIM_CHANNEL_1, TIM_ICPSC_DIV1);
+                    Encoder.PSCR1 = 1;
+                    Encoder.skip_next_calcA = true;
+            }
+        }
+        // ---------------------------------------------------------
+        // CHANNEL 3 (ENCODER B)
+        // ---------------------------------------------------------
+        else if(htim->Channel == HAL_TIM_ACTIVE_CHANNEL_3)
+        {
+            // Same as last
+            Encoder.watchdog_timerB = 0;
+            Encoder.capture_valueB = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_3);
+            uint32_t diff = Encoder.capture_valueB - Encoder.prev_capture_valueB;
+            Encoder.prev_capture_valueB = Encoder.capture_valueB;
+            
+            if(diff == 0) return;
+            
+            if(Encoder.PSCR3 == 1 && Encoder.skip_next_calcB == 0)
+            {
+                Encoder.speed_mpsB = Encoder.clk_Hz * 2 * PI * WHEEL_DIAMETER / (1 + Encoder.PSC) / diff / Encoder.CPR;
+            }
+            else if(Encoder.PSCR3 == 8 && Encoder.skip_next_calcB == 0)
+            {
+                Encoder.speed_mpsB = Encoder.clk_Hz * 2 * PI * WHEEL_DIAMETER * 8 / (1 + Encoder.PSC) / diff / Encoder.CPR;
+            }
+            else
+            {
+                Encoder.skip_next_calcB = false;
+            }
+            
+            Encoder.dirB = LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_3) ? 0 : 1;
+            
+            if(Encoder.speed_mpsB > HIGH_SPEED_THRESHOLD && Encoder.PSCR3 == 1)
+            {
+                __HAL_TIM_SET_ICPRESCALER(htim, TIM_CHANNEL_3, TIM_ICPSC_DIV8);
+                Encoder.PSCR3 = 8;
+                Encoder.skip_next_calcB = true;
+            }
+            else if(Encoder.speed_mpsB < LOW_SPEED_THRESHOLD && Encoder.PSCR3 == 8)
+            {
+                __HAL_TIM_SET_ICPRESCALER(htim, TIM_CHANNEL_3, TIM_ICPSC_DIV1);
+                Encoder.PSCR3 = 1;
+                Encoder.skip_next_calcB = true;
+            }
+        }
+    }
 }
 
 // HAL_ADC_Conv...: 
 //Conversion complete callback in non blocking mode; overdefined.
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 {
-//#ifndef MATLAB_MEX_FILE
     // Check the adc instance
     if (hadc == VBAT_ADC)
     {
@@ -60,10 +229,24 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
         uint16_t adcVal = HAL_ADC_GetValue(VBAT_ADC);
         batteryVoltage =  adcVal * ADC_TO_VBAT_MULTIPLIER + ADC_TO_VBAT_OFFSET;
     }
-//#endif
 }
 
 #endif
+
+// Update_Battery_Logic
+// Used to call an ADC interrupt upon pressing the button.
+void Update_Battery_Logic(bool buttonValue) {
+    #if !defined(SLCC_SIMULATION) && !defined(MATLAB_MEX_FILE)
+    //read the hardware
+    if (buttonValue == 0) {
+        HAL_ADC_Start(VBAT_ADC);
+    }
+    #else
+        // For the simulation, do nothing
+    #endif
+}
+
+
 
 ////    Functions  ////
 
@@ -81,7 +264,7 @@ uint32_t infrared(){
     // ELSIF: The mesured distance is invalid
     else if(InfraSensor.distance_mm < 0)
         InfraSensor.valid = false;
-    // ELSE: The measured distance is out of range
+    // ELSE: The measured distance is in range
     else
     {
         InfraSensor.oor = false;
@@ -107,28 +290,122 @@ uint32_t infrared(){
 }
 
 
-
-
-
-uint16_t readBatteryVoltage(){
+// Display the Battery voltage
+uint16_t readBatteryVoltage(bool buttonValue)
+{
+    Update_Battery_Logic(buttonValue);
     return batteryVoltage;
 }
 
+double getSpeedA()
+{
+    // Decide if the wheels are stationary; For how many cycles, can be set by the MAXIMUM_IDLE_TIME
+    if(Encoder.watchdog_timerA < MAXIMUM_IDLE_TIME)
+    {
+        Encoder.watchdog_timerA++;
+        return Encoder.dirA == 1 ? Encoder.speed_mpsA : -Encoder.speed_mpsA;
+    }
+    else
+    {
+        Encoder.speed_mpsA = 0.0f;
+        return 0;
+    }
+}
+
+double getSpeedB()
+{
+    // Same as last
+    if(Encoder.watchdog_timerB < MAXIMUM_IDLE_TIME)
+    {
+        Encoder.watchdog_timerB++;
+        return Encoder.dirB == 1 ? Encoder.speed_mpsB : -Encoder.speed_mpsB;
+    }
+    else
+    {
+        Encoder.speed_mpsB = 0.0f;
+        return 0;
+    }
+}
+
+RGB_t getRGB()
+{
+    RGB_t result = {0};
+    uint16_t sR = 65535;
+    uint16_t sG = 65535;
+    uint16_t sB = 65535;
+
+    if (ColorSensor.pRed != 0) {
+        sR = SCALE / ColorSensor.pRed;
+    }
+    if (ColorSensor.pGreen != 0) {
+        sG = SCALE / ColorSensor.pGreen;
+    }
+    if (ColorSensor.pBlue != 0) {
+        sB = SCALE / ColorSensor.pBlue;
+    }
+
+    result.R = (sR - R_MIN) * 255 / (R_MAX - R_MIN);
+    resutl.G = (sG - G_MIN) * 255 / (G_MAX - G_MIN);
+    result.B = (sB - B_MIN) * 255 / (B_MAX - B_MIN);
+    return result;
+}
+
+// Functions that doesn't need to be seen by simulink
+#if !defined(SLCC_SIMULATION) && !defined(MATLAB_MEX_FILE)
+// Helper function for dynamically setting the prescaler division ratio for each timer channel
+uint8_t Get_Actual_Ratio(uint32_t hal_macro) {
+    switch (hal_macro) {
+        case TIM_ICPSC_DIV8:
+            return 8;
+        case TIM_ICPSC_DIV4:
+            return 4;
+        case TIM_ICPSC_DIV2:
+            return 2;
+        case TIM_ICPSC_DIV1:
+        default:
+            return 1;
+    }
+}
+#endif
+
 // Timer_Init_Custom
-// Starts the interrupt handling of the timers
+// Starts the interrupt handling of the timers; needs to be seen by simulink!!
 // Also: reads the clock speed and certain register values from the generated code 
 void Timer_Init_Custom(void){
 #if !defined(SLCC_SIMULATION) && !defined(MATLAB_MEX_FILE)
     // Infrared sensor Part //
         // Start Input capture
-        HAL_TIM_IC_Start_IT(&htim4,TIM_CHANNEL_1);
+            // Rising edge
+        HAL_TIM_IC_Start_IT(&htim4,TIM_CHANNEL_1);  
+            // Falling edge
         HAL_TIM_IC_Start_IT(&htim4,TIM_CHANNEL_2);
         //Update the PSC from the generated STM32CubeMX code :))))
         InfraSensor.PSC = htim4.Init.Prescaler;
         //Update the clk_MHz from the generated ... :)
         uint32_t apb1_freq_hz = HAL_RCC_GetPCLK1Freq();
         InfraSensor.clk_MHz = (apb1_freq_hz * 2) / 1000000; // The multiplication is required because of the clock configuration (->/2->*2-> Problem!; APB1)
-    //Battery Indicator
-        HAL_ADC_Start_IT(VBAT_ADC);
+
+    // Encoder Part //
+        // EncoderA (1,2)
+        HAL_TIM_IC_Start_IT(&htim5,TIM_CHANNEL_1);
+        // EncoderB (1,2)
+        HAL_TIM_IC_Start_IT(&htim5,TIM_CHANNEL_3);
+
+        Encoder.PSC = htim5.Init.Prescaler; // This should be 999, so we get 100 kHz, so we get the best resolution
+        Encoder.clk_Hz = InfraSensor.clk_MHz*1000000;   // This yould be in MHz
+
+        // Get the raw HAL bitmasks
+        uint32_t rawPSC1 = __HAL_TIM_GET_ICPRESCALER(&htim5, TIM_CHANNEL_1);
+        uint32_t rawPSC3 = __HAL_TIM_GET_ICPRESCALER(&htim5, TIM_CHANNEL_3);
+
+        // Convert them to integers (1, 2, 4, or 8) and store them
+        Encoder.PSCR1 = Get_Actual_Ratio(rawPSC1);
+        Encoder.PSCR3 = Get_Actual_Ratio(rawPSC3);
+
+    // Color sensor Part //
+        HAL_TIM_IC_Start_IT(&htim4,TIM_CHANNEL_3);
+        ColorSensor.PSC = htim4.Init.Prescaler;
+        ColorSensor.clk_Hz = (apb1_freq_hz * 2);
+
 #endif
 }
--- C_kod/SensorActuator.h
+++ C_kod/SensorActuator.h
@@ -4,18 +4,19 @@
 
 #include <stdint.h>
 #include <stdbool.h>
-/*
-#ifndef MATLAB_MEX_FILE
-    #include "main.h"
-#else
-    typedef struct TIM_HandleTypeDef TIM_HandleTypeDef; 
-    typedef struct ADC_HandleTypeDef ADC_HandleTypeDef;
-#endif
-//*/
-//Constants
+
+// Global variables
 
 extern uint32_t last_distance;
 extern uint32_t batteryVoltage;
+extern uint32_t last_speed;
+// Different Color Channels for the Color Sensor
+typedef enum {
+    COLOR_RED   = 0,
+    COLOR_BLUE  = 1,
+    COLOR_GREEN = 2,
+    COLOR_CLEAR = 3
+} ColorChannel_t;
 
 // Structs
 
@@ -29,20 +30,92 @@ typedef struct {
     bool valid;
 } infraredTypedef;
 
+typedef struct{
+    uint32_t capture_valueA;
+    uint32_t prev_capture_valueA;
+    uint32_t capture_valueB;
+    uint32_t prev_capture_valueB;
+    double speed_mpsA;
+    double speed_mpsB;
+    uint32_t clk_Hz;
+    uint16_t PSC;
+    uint16_t CPR;
+    uint8_t PSCR1;
+    uint8_t PSCR3;
+    bool skip_next_calcA;
+    bool skip_next_calcB;
+    uint8_t dirA;
+    uint8_t dirB;
+    uint8_t watchdog_timerA;
+    uint8_t watchdog_timerB;
+} encoderTypedef;
+
+typedef struct{
+    uint32_t sum;
+    uint8_t counter;
+    uint16_t capture_value;
+    uint16_t prev_capture_value;
+    uint16_t PSC;
+    uint32_t clk_Hz;
+    float pRed;
+    float pBlue;
+    float pGreen;
+    ColorChannel_t Color;
+    bool stepFlag;
+} colorSensorTypedef;
+
+typedef struct{
+    uint16_t R;
+    uint16_t G;
+    uint16_t B;
+} RGB_t;
+
 // Defines
 
-#define InrfaSamples 8
-#define OORDISTANCE 2000
-#define ADC_TO_VBAT_MULTIPLIER      (3300 * 5 / 4096)
-#define ADC_TO_VBAT_OFFSET          220
+    // For the Distance Measuring, not needed, possible implementation of averaging
+    #define InrfaSamples 8
+    //Set OOEDISTANCE to set the out of range distance of the infrared sensor; [mm]
+    #define OORDISTANCE 2000
+    //For the ADC
+    #define ADC_TO_VBAT_MULTIPLIER      (3300 * 5 / 4096)
+    #define ADC_TO_VBAT_OFFSET        220
+    //For the encoders
+    #define PI 3.14159265f
+    #define WHEEL_DIAMETER 0.0325f     //[m]
+    #define HIGH_SPEED_THRESHOLD 0.2f //[m/s]
+    #define LOW_SPEED_THRESHOLD 0.1f //[m/s]
+    #define MAXIMUM_IDLE_TIME 5  //[interrupt cycles]
+
+    // ColorSensor
+    #define SCALE 10000
+
+    #define R_P_MAX 1100
+    #define R_P_MIN 70
+
+    #define G_P_MAX 1550
+    #define G_P_MIN 70
+
+    #define B_P_MAX 1800
+    #define B_P_MIN 65
+
+    #define R_MIN (SCALE / R_P_MAX)
+    #define R_MAX (SCALE / R_P_MIN)
+    #define G_MIN (SCALE / G_P_MAX)
+    #define G_MAX (SCALE / G_P_MIN)
+    #define B_MIN (SCALE / B_P_MAX)
+    #define B_MAX (SCALE / B_P_MIN)
 
 // Functions
 
-uint32_t infrared();
-/*
-void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim);
-*/
-void Timer_Init_Custom(void);
-uint16_t readBatteryVoltage();
 
+uint32_t infrared();
+void Timer_Init_Custom(void);
+uint16_t readBatteryVoltage(bool buttonValue);
+void Update_Battery_Logic(bool buttonValue);
+double getSpeedA();
+double getSpeedB();
+RGB_t getRGB();
+
+//Global variables extended
+extern volatile encoderTypedef Encoder;
 #endif /* SENSORACTUATOR_H */
